1682e6237e052b3266bbf7f40877f4d8c4ba040c
dankkunal <kunalbehlba@gmail.com>
2023-06-07 02:18:55 +0530

    Initial commit

---
+++ helpers/authHelper.js
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (password) => {
+  try {
+    const saltrounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltrounds);
+    return hashedPassword;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+export const comparePassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+
+//Configuring dotenv
+dotenv.config();
+
+//Database Connection
+connectDB();
+
+//REST Object
+const app = express();
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(morgan("dev"));
+
+//ROUTES
+app.use("/api/v1/auth", authRoutes);
+
+//REST API
+app.get("/", (req, res) => {
+  res.send("<h1><center>HELLO WORLD</center></h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
