f9e6bce9307a846269baa92e8add8ca5a6249c9f
dankkunal <kunalbehlba@gmail.com>
2023-06-17 22:26:28 +0530

    17.06.2023

---
+++ client/src/hooks/useCategory.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useCategory() {
+  const [categories, setCategories] = useState([]);
+
+  // Get all categories
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/category/get-categories`
+      );
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return { categories };
+}
