9dfc473bec23f8b4cda269ed5c8dcc9db8c461f9
dankkunal <kunalbehlba@gmail.com>
2023-06-21 00:16:24 +0530

    ..

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Configuring dotenv
 dotenv.config();
